0263c152820b382b35fa659a0654e353d8de6452
Arata Yano <aratayano00@gmail.com>
2019-08-06 15:18:50 +0200

    paramsum

--- paramsum.c
+++ paramsum.c
@@ -3,43 +3,40 @@
 /*                                                        :::      ::::::::   */
 /*   paramsum.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ayano <ayano@student.42.fr>                +#+  +:+       +#+        */
+/*   By: ayano <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/06/19 13:27:35 by ayano             #+#    #+#             */
-/*   Updated: 2019/06/19 13:41:38 by ayano            ###   ########.fr       */
+/*   Created: 2019/08/06 15:07:50 by ayano             #+#    #+#             */
+/*   Updated: 2019/08/06 15:15:24 by ayano            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-void    ft_putchar(char c)
+void	ft_putchar(char c)
 {
-    write(1, &c, 1);
+	write(1, &c, 1);
 }
 
-void    ft_putnbr(int n)
+void	ft_putnbr(int nb)
 {
-    if (n < 0)
-    {
-        ft_putchar('-');
-        n = n * -1;
-    }
-    else if (n > 10)
-        ft_putnbr(n / 10);
-    ft_putchar((n % 10) + 48);
+	if (nb == -2147483648)
+		write(1, "-2147483648", 11);
+	if (nb > 9)
+		ft_putnbr(nb / 10);
+	ft_putchar(nb % 10 + '0');
 }
 
-int     main(int argc, char **argv)
+
+
+int		main(int argc, char **argv)
 {
-    if (argc < 2)
-    {
-        write(1, "0\n", 2);
-        return (0);
-    }
-    else if (argc >= 2)
-    {
-        ft_putnbr(argc - 1);
-    }
-    write(1, "\n", 1);
-    return (0);
+	char *str;
+	str = argv[1];
+
+	if (argc > 1)
+		ft_putnbr(argc - 1);
+	else
+		write(1, "0", 1);
+	write(1, "\n", 1);
+	return (0);
 }
